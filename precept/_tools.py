ca0d37e4d4fd4b89c2d9ccef2fcbc8ca6c6a196f
t4rk <t4rk@outlook.com>
2019-03-07 22:27:06 -0500

    :sparkles: Add format_table

---
+++ precept/_tools.py
@@ -0,0 +1,60 @@
+import sys
+
+__all__ = [
+    'chunk_list',
+    'is_windows',
+    'format_table',
+    'print_table'
+]
+
+
+def chunk_list(data, n):
+    for i in range(0, len(data), n):
+        yield data[i:i+n]
+
+
+def is_windows():
+    return sys.platform == 'win32'
+
+
+def format_table(data, formatting=None):
+    # 2 padding + '|'
+    max_len = max(len(x) for x in data) + 3
+    if max_len % 2 == 0:
+        # Revert uneven for a bit nicer look.
+        max_len += 1
+    min_len = len(data) * max_len
+    r = 79 / max_len
+
+    if r - int(r) > 0:
+        col = int(r) - 1
+    else:
+        col = int(r)
+
+    if min_len < 79:
+        row_len = min_len + len(data) + 1
+    else:
+        row_len = max_len * col
+        # Add the columns + 1 for the first '|'
+        row_len += col + 1
+
+    if not formatting:
+        formatting = lambda e: e
+
+    chunks = chunk_list(data, col)
+    rows = ['-' * row_len]
+    for chunk in chunks:
+        rows.append(
+            '|{}|'.format(
+                '|'.join(
+                    formatting(c.center(max_len))
+                    for c in chunk
+                )
+            )
+        )
+        rows.append('-' * row_len)
+    return rows
+
+
+def print_table(data, formatting=None, file=sys.stdout):
+    print('\n'.join(format_table(data, formatting,)), file=file)
