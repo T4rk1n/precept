07c7e80e95ae7c0b98bb27dd5a4f246911cdf8b7
t4rk <t4rk@outlook.com>
2019-05-01 21:54:23 -0400

    :white_check_mark: Add test_immutable.

---
+++ tests/test_immutable.py
@@ -0,0 +1,35 @@
+import pytest
+
+from precept import ImmutableDict
+
+
+def test_immutable_dict():
+    data = ImmutableDict(foo='bar', bar='foo', n=1)
+    assert 'foo' in data
+    assert data.get('bar') == 'foo'
+    assert data['n'] == 1
+    assert len(data) == 3
+
+    with pytest.raises(TypeError):
+        data['foo'] = 'not foo'
+
+
+def test_immutable_props():
+    class TestDict(ImmutableDict):
+        def __init__(self, foo, bar, keyword='keyword'):
+            super(TestDict, self).__init__(foo=foo, bar=bar, keyword=keyword)
+
+    first = TestDict('foo', 'bar')
+    assert first.foo == 'foo'
+    assert first.bar == 'bar'
+    assert first.keyword == 'keyword'
+    assert len(first) == 3
+
+    with pytest.raises(TypeError) as context:
+        first.foo = 'bar'
+
+    assert 'TestDict.foo is immutable' in str(context.value)
+
+    second = TestDict(1, 2, keyword='foobar')
+    assert second.keyword == 'foobar'
+    assert len(second) == 3
