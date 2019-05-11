77aa8d03b1001203d97f938ee0cec1edc05b0c20
t4rk <t4rk@outlook.com>
2019-05-10 20:02:21 -0400

    :white_check_mark: Add test_spinner.

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import asyncio
+
+
+# noinspection PyProtectedMember
+@pytest.mark.tryfirst
+def pytest_pyfunc_call(pyfuncitem):
+    """Run tests marked as async in the loop."""
+    for marker in pyfuncitem.own_markers:
+        if marker.name == 'async_test':
+            loop = asyncio.get_event_loop()
+            task = loop.create_task(pyfuncitem.obj(**{
+                k: pyfuncitem.funcargs[k]
+                for k in pyfuncitem._fixtureinfo.argnames
+            }))
+            loop.run_until_complete(task)
+            return True
