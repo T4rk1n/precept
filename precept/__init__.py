49593a7b555128246fc0022f181693a024cfa48b
t4rk <t4rk@outlook.com>
2019-05-22 20:53:59 -0400

    :sparkles: Add config_factory.

--- precept/__init__.py
+++ precept/__init__.py
@@ -4,4 +4,6 @@ from ._keyhandler import KeyHandler, Keys, getch  # noqa: F401
 from ._printing import *  # noqa: F401, F403
 from ._tools import *  # noqa: F401, F403
 from ._immutable import *  # noqa: F401, F403
-from ._configs import ConfigProperty, Config, ConfigFormat, Nestable
+from ._configs import (
+    ConfigProperty, Config, ConfigFormat, Nestable, config_factory
+)
