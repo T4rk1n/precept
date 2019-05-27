5bef7014338705f63384bda2fb566366b016a4ef
t4rk <t4rk@outlook.com>
2019-05-26 21:15:50 -0400

    :hammer: Refactor console stuff to console package.

--- precept/__init__.py
+++ precept/__init__.py
@@ -1,7 +1,5 @@
 from ._version import __version__  # noqa: F401
 from ._cli import Precept, Command, Argument  # noqa: F401
-from ._keyhandler import KeyHandler, Keys, getch  # noqa: F401
-from ._printing import *  # noqa: F401, F403
 from ._tools import *  # noqa: F401, F403
 from ._immutable import *  # noqa: F401, F403
 from ._configs import (  # noqa: F401
