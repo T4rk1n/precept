39f5ea4ec5d5d724d33ec6a15a13f15a5070df8a
t4rk <t4rk@outlook.com>
2019-05-27 20:46:34 -0400

    :white_check_mark: Add test_progress_bar.

--- tests/test_printing.py
+++ tests/test_printing.py
@@ -1,6 +1,8 @@
+import itertools
+import math
 import pytest
 
-from precept.console import print_table, spinner
+from precept.console import print_table, spinner, progress_bar
 
 
 @pytest.mark.async_test
@@ -28,6 +30,42 @@ async def test_spinner(capsys):
         message=lambda: f'{messages[namespace["i"]]} ... '
     )
     assert namespace['i'] == len(messages)
+
+
+@pytest.mark.async_test
+@pytest.mark.parametrize(
+    'max_value, dents',
+    list(itertools.product(
+        [50, 77.77, 88, 100, 200, 32422], [50, 20, 18, 99, 35, 80]
+    ))
+)
+async def test_progress_bar(capsys, max_value, dents):
+    max_value = 50
+
+    namespace = {
+        'i': 0,
+    }
+
+    def value_formatter(value, value_max):
+        return f' {value/value_max * 100:.2f}%'
+
+    def value_func():
+        if namespace['i'] > 0:
+            out, err = capsys.readouterr()
+            clean = out.strip().split(']')[0].split('[')[-1]
+            num = math.ceil(namespace['i'] / max_value * dents)
+            assert num * '#' == clean[:num]
+            assert clean[num:] == (dents - num) * '-'
+        namespace['i'] += 1
+        return namespace['i']
+
+    await progress_bar(
+        value_func,
+        max_value=max_value,
+        value_formatter=value_formatter,
+        file=None,
+        dents=dents,
+    )
 
 
 def test_table(capsys):
